e4c07959f9a74fa500055b28d26614cff2df93eb
lberganton <lucasbberganton@gmail.com>
2026-05-31 17:06:25 -0300

    Remove o plugin de relative-motions no Yazi pois está quebrado

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,1 +1,1 @@
-require("relative-motions"):setup({ show_numbers="relative_absolute", show_motion = true, enter_mode ="first" })
+-- require("relative-motions"):setup({ show_numbers="relative_absolute", show_motion = true, enter_mode ="first" })
